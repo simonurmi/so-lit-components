b4c2d9040f1c0341c1c19ef9466156052661112d
Simo Nurmi <simo.nurmi@solita.fi>
2019-02-15 15:35:45 +0200

    Add color property

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import styles from './Button.module.css';
 
 const Button = (props) => {
-  const { children, type } = props;
+  const { children, color, type } = props;
+  const classes = classNames({
+    [styles.Button]: true,
+    [styles['Button--light']]: color === 'light',
+  });
   return (
-    <button className={styles.Button} type={type}>
+    // eslint-disable-next-line react/button-has-type
+    <button className={classes} type={type}>
       {children}
     </button>
   );
@@ -13,10 +19,12 @@ const Button = (props) => {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  color: PropTypes.node,
   type: PropTypes.string,
 };
 
 Button.defaultProps = {
+  color: 'dark',
   type: 'button',
 };
 
