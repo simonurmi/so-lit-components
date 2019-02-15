f9b211c0b95e2c19bcbe98e51f3de7d265e32134
Simo Nurmi <simo.nurmi@solita.fi>
2019-02-15 15:35:36 +0200

    Add padding property

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -4,23 +4,35 @@ import classNames from 'classnames';
 import styles from './Background.module.css';
 
 const Background = (props) => {
-  const { children, color } = props;
+  const {
+    children, color, image, padding,
+  } = props;
   const classes = classNames({
     [styles.Background]: true,
     [styles['Background--primary']]: color === 'primary',
     [styles['Background--light']]: color === 'light',
     [styles['Background--dark']]: color === 'dark',
+    [styles['Background--image']]: !!image,
+    [styles['Background--padding-1']]: padding === 1,
   });
-  return <div className={classes}>{children}</div>;
+  return (
+    <div className={classes} style={{ backgroundImage: `url(${image})` }}>
+      {children}
+    </div>
+  );
 };
 
 Background.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.string,
+  image: PropTypes.string,
+  padding: PropTypes.number,
 };
 
 Background.defaultProps = {
-  color: '',
+  color: null,
+  image: null,
+  padding: null,
 };
 
 export default Background;
