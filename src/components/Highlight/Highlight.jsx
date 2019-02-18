f28bd7ee64694d07f0022c08b877876505265ad3
Simo Nurmi <simo.nurmi@solita.fi>
2019-02-18 14:43:00 +0200

    Use Intrinsic with image

--- src/components/Highlight/Highlight.jsx
+++ src/components/Highlight/Highlight.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import styles from './Highlight.module.css';
+import Intrinsic from '../Intrinsic/Intrinsic';
 
 const Highlight = (props) => {
   const {
-    cta, img, title, text,
+    alt, callToAction, img, title, text,
   } = props;
   return (
     <div className={styles.Highlight}>
-      <img alt="" src={img} />
-      <h3>{title}</h3>
-      <p>{text}</p>
-      <Button>{cta}</Button>
+      <div className={styles.Highlight__Image}>
+        <Intrinsic ratioX={16} ratioY={9}>
+          <img alt={alt} src={img} />
+        </Intrinsic>
+      </div>
+      <h3 className={styles.Highlight__Title}>{title}</h3>
+      <p className={styles.Highlight__Text}>{text}</p>
+      <Button>{callToAction}</Button>
     </div>
   );
 };
 
 Highlight.propTypes = {
-  cta: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  callToAction: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
